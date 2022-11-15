489d6d68c9cd30c1a544c830918076b5d31a72c2
xeluxee <88047141+xeluxee@users.noreply.github.com>
2022-11-15 19:25:13 +0100

    fix: keep compatibility with Neovim < 0.7.2 and update README.md

--- lua/competitest/init.lua
+++ lua/competitest/init.lua
@@ -117,7 +117,7 @@ local default_config = {
 		python = { exec = "python", args = { "$(FNAME)" } },
 		java = { exec = "java", args = { "$(FNOEXT)" } },
 	},
-	multiple_testing = -1, -- how many testcases to run at the same time. Set it to 0 to run all them together, -1 to use the number of available cpu cores, or any positive number to run how many testcases you want
+	multiple_testing = -1, -- how many testcases to run at the same time. Set it to 0 to run all them together, -1 to use amount of available parallelism, or any positive number to run how many testcases you want
 	maximum_time = 5000, -- maximum time (in milliseconds) given to a process. If it's excedeed process will be killed
 	output_compare_method = "squish", -- "exact", "squish" or custom function returning true if comparison is valid
 
