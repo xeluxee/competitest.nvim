e0821bb1658edf7a73a758b5de7c45de6ee3fe10
Francesco Derme <francesco.derme02@gmail.com>
2025-03-28 17:15:52 +0100

    changed width and height

--- lua/competitest/runner_ui/init.lua
+++ lua/competitest/runner_ui/init.lua
@@ -308,8 +308,8 @@ function RunnerUI:show_viewer_popup(window_name)
 			},
 			relative = "editor",
 			size = {
-				width = math.floor(vim_width * self.runner.config.runner_ui.viewer.width),
-				height = math.floor(vim_height * self.runner.config.runner_ui.viewer.height),
+				width = math.floor(vim_width * self.runner.config.runner_ui.viewer.width + 1),
+				height = math.floor(vim_height * self.runner.config.runner_ui.viewer.height + 1),
 			},
 			position = "50%",
 			win_options = {
